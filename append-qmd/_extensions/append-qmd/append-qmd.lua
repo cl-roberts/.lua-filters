bdeb98d7251d9028f0c16621364e866d62f50d96
CL Roberts <cl.roberts@alaska.gov>
2023-06-29 09:24:16 -0800

    clarify pandoc reader format

--- append-qmd/_extensions/append-qmd/append-qmd.lua
+++ append-qmd/_extensions/append-qmd/append-qmd.lua
@@ -3,11 +3,12 @@
 
 function Pandoc(el)
     local current_dir = pandoc.path.directory(PANDOC_SCRIPT_FILE)
+    local format = "markdown"
     for dir in io.popen("ls " .. current_dir):lines() do
         if (string.match(dir, ".qmd") and (current_dir .. "/" .. dir ~= PANDOC_SCRIPT_FILE)) then
             local filepath = current_dir .. "/" .. dir
             local content = pandoc.read(
-                  io.open(filepath):read "*a", nil, 
+                  io.open(filepath):read "*a", format, 
                   PANDOC_READER_OPTIONS
                 ).blocks
             el.blocks:extend(content)
