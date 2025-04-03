8640014fea1c8c07c0facce89f3e04048a7b0208
CL Roberts <cl.roberts@alaska.gov>
2025-04-03 15:13:31 -0700

    appendix header customizability

--- code-appendix/_extensions/code-appendix/code-appendix.lua
+++ code-appendix/_extensions/code-appendix/code-appendix.lua
@@ -6,26 +6,40 @@ appendix = {}
 function CodeBlock(el)
 
   if el.classes[2] == "cell-code" then 
-
     table.insert(appendix, el)
     return {}
-
   else 
-
     return el
-
   end
 
 end
 
--- append pandoc document with source code
-function Pandoc(el)
-  
-  appendixHeader = pandoc.Header(1, pandoc.Str "test")
-  el.blocks:extend(pandoc.List({appendixHeader}))
-  el.blocks:extend(pandoc.List(appendix))
+-- reads yaml to obtain desired header for code appendix
+function Meta(m)
 
-  return el
+  if m["code-appendix-title"] ~= nil then
+    codeAppendixTitle = pandoc.utils.stringify(m["code-appendix-title"])
+  else
+    codeAppendixTitle = pandoc.Str("Code")
+  end
+
+  if m["code-appendix-header-level"] ~= nil then
+    codeAppendixHeaderLevel = tonumber(pandoc.utils.stringify(m["code-appendix-header-level"]))
+  else
+    codeAppendixHeaderLevel = 1
+  end
+
+  return m
 
 end
 
+-- append pandoc document with source code
+function Pandoc(doc)
+  
+  appendixHeader = pandoc.Header(codeAppendixHeaderLevel, codeAppendixTitle)
+  doc.blocks:extend(pandoc.List({appendixHeader}))
+  doc.blocks:extend(pandoc.List(appendix))
+
+  return doc
+
+end
