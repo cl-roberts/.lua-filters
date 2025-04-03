e5927a722986b38c99e45844f5d3ea1bd0769499
CL Roberts <cl.roberts@alaska.gov>
2025-04-03 16:49:05 -0700

    feature to write source code to txt file

--- code-appendix/_extensions/code-appendix/code-appendix.lua
+++ code-appendix/_extensions/code-appendix/code-appendix.lua
@@ -15,6 +15,7 @@ function CodeBlock(el)
 end
 
 -- reads yaml to obtain desired header for code appendix
+-- writes appendix to txt file if desired
 function Meta(m)
 
   if m["code-appendix-title"] ~= nil then
@@ -27,6 +28,21 @@ function Meta(m)
     codeAppendixHeaderLevel = tonumber(pandoc.utils.stringify(m["code-appendix-header-level"]))
   else
     codeAppendixHeaderLevel = 1
+  end
+
+  -- write appendix to text file
+  if m["code-appendix-write"] ~= nil then
+
+    codeAppendixWrite = pandoc.utils.stringify(m["code-appendix-write"])
+
+    if codeAppendixWrite == "true" then
+      local f = io.open(pandoc.system.get_working_directory() .. '/code-appendix.txt', 'w')
+      for index, value in ipairs(pandoc.List(appendix)) do
+        f:write(pandoc.List(appendix)[index].text .. "\n\n")
+      end
+      f:close()
+    end
+  
   end
 
   return m
