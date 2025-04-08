9031d8c83dfb198c2208bb31fdc3735f7c488dbb
CL Roberts <cl.roberts@alaska.gov>
2025-04-08 12:08:14 -0700

    enable writing to any file type, implement code-appendix-write

--- code-appendix/_extensions/code-appendix/code-appendix.lua
+++ code-appendix/_extensions/code-appendix/code-appendix.lua
@@ -15,7 +15,7 @@ function CodeBlock(el)
 end
 
 -- reads yaml to obtain desired header for code appendix
--- writes appendix to txt file if desired
+-- writes appendix to file
 function Meta(m)
 
   if m["code-appendix-title"] ~= nil then
@@ -30,19 +30,39 @@ function Meta(m)
     codeAppendixHeaderLevel = 1
   end
 
-  -- write appendix to text file
   if m["code-appendix-write"] ~= nil then
+    codeAppendixWrite = m["code-appendix-write"]
+  else
+    codeAppendixWrite = true
+  end
 
-    codeAppendixWrite = pandoc.utils.stringify(m["code-appendix-write"])
+  -- write appendix to file
+  if m["code-appendix-file"] ~= nil then
 
-    if codeAppendixWrite == "true" then
-      local f = io.open(pandoc.system.get_working_directory() .. '/code-appendix.txt', 'w')
-      for index, value in ipairs(pandoc.List(appendix)) do
-        f:write(pandoc.List(appendix)[index].text .. "\n\n")
-      end
-      f:close()
+    codeAppendixFile = pandoc.utils.stringify(m["code-appendix-file"])    
+    codeAppendixExt = codeAppendixFile:match("%.[^.]+$")    
+
+    if codeAppendixExt == nil then
+      codeAppendixExt = ".md"
+      codeAppendixFile = codeAppendixFile .. codeAppendixExt
     end
-  
+
+    local f = io.open(pandoc.system.get_working_directory() .. "/" .. codeAppendixFile, 'w')
+ 
+    for index, value in ipairs(pandoc.List(appendix)) do
+      if codeAppendixExt == ".md" or codeAppendixExt == ".qmd" then
+        f:write(
+          "```" .. "\n" .. pandoc.List(appendix)[index].text .. "\n" .. "```" .. "\n\n"
+        )
+      else 
+        f:write(
+          pandoc.List(appendix)[index].text .. "\n\n"
+        )  
+      end
+    end
+    
+    f:close()
+
   end
 
   return m
@@ -51,10 +71,12 @@ end
 
 -- append pandoc document with source code
 function Pandoc(doc)
-  
-  appendixHeader = pandoc.Header(codeAppendixHeaderLevel, codeAppendixTitle)
-  doc.blocks:extend(pandoc.List({appendixHeader}))
-  doc.blocks:extend(pandoc.List(appendix))
+
+  if codeAppendixWrite then
+    appendixHeader = pandoc.Header(codeAppendixHeaderLevel, codeAppendixTitle)
+    doc.blocks:extend(pandoc.List({appendixHeader}))
+    doc.blocks:extend(pandoc.List(appendix))
+  end
 
   return doc
 
